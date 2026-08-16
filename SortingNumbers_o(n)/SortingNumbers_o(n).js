7a0d748a231dba307975ec57f5faed3dabb1d64d
kavyaankem <ankemkavyaa03@gmail.com>
2026-08-16 19:50:35 +0530

    O(n), Number sorting using while loop

--- SortingNumbers_o(n)/SortingNumbers_o(n).js
+++ SortingNumbers_o(n)/SortingNumbers_o(n).js
@@ -13,7 +13,7 @@ function onclickToSort() {
     document.getElementById("divSort").innerHTML = sorting;
 }
 
-function onClickToWhileLoop() {
+function onClickToAddWhileLoop() {
     debugger;
     let target = 9;
     let left = 0;
@@ -33,6 +33,54 @@ function onClickToWhileLoop() {
         else if(sum > target) {
             right--;
         }
-        document.getElementById("divWhileLoop").innerHTML = count;
+        document.getElementById("divAdd").innerHTML = count;
+    }
+}
+
+function onClickToSubWhileLoop() {
+    debugger;
+    let target = 0;
+    let left = 0;
+    let right = array.length - 1;
+    let sub = [];
+    let count = 0;
+    while(left < right){
+        sub = ((array[right] - array[left]));
+        if(sub == target){
+            left++;
+            right--;
+            count++;
+        }
+        else if(sub < target){
+            left++;
+        }
+        else if(sub > target){
+            right--;
+        }
+        document.getElementById("divSub").innerHTML = count;
+    }
+}
+
+function onClickToMulWhileLoop() {
+    debugger;
+    let target = 10;
+    let left = 0;
+    let right = array.length - 1;
+    let mul = [];
+    let count = 0;
+    while(left < right){
+        mul = ((array[left] * array[right]));
+        if(mul == target){
+            left++;
+            right--;
+            count++;
+        }
+        else if(mul < target) {
+            left++;
+        }
+        else if(mul > target){
+            right--;
+        }
+        document.getElementById("divMul").innerHTML = count;
     }
 }
