1890752f90aeca69a05a79352ef9735a0391aa88
kavyaankem <ankemkavyaa03@gmail.com>
2026-08-31 18:09:16 +0530

    sliding array

---
+++ SlidingArray_o(n2)/SlidingArray_o(n2).js
@@ -0,0 +1,17 @@
+let array = [2, 4, 7, 9, 10, 4, 3, 1];
+let bigArray = [];
+let arraySum = [];
+function onClickToSlide(){
+    debugger;
+    let k = 3;
+    for(i=0 ; i<array.length ; i++){
+        let subArray = [];
+        let arraySum = 0;
+        for(j=i ; j<i+k ; j++){
+            
+        }
+        bigArray.push(subArray);
+        console.log(subArray);
+        console.log(bigArray);
+    }
+}
